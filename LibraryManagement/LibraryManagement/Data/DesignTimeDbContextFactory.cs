e320a73c76308b2cd98dbd84c528886476e4c0d8
Pujitha-514 <pujithachitirala88@gmail.com>
2025-11-22 20:06:03 +0530

    Created a new Library project...

---
+++ LibraryManagement/LibraryManagement/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Data
+{
+    public class DesignTimeDbContextFactory
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            // Use your MySQL connection string here
+            var connectionString = "server=localhost;port=3306;database=LibraryDb;user=root;password=yourpassword";
+
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
